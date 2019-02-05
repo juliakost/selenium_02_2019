17a3b774a3a16b79b677ff064140add63765554d
MICROGEN-PL01\yuliia.kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-02-05 23:29:47 +0100

    Задание 1. Подготовьте инфраструктуру

---
+++ selenium/pl_yk/src/test/java/training/selenium/MyFirstTest.java
@@ -0,0 +1,34 @@
+package training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.navigate().to("http://www.google.com");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
