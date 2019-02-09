a74cf728e9103103394ff4474f3960121a3f16dc
MICROGEN-PL01\yuliia.kostitsyna <yuliia.kostitsyna@aptitudesoftware.com>
2019-02-10 00:10:18 +0100

    Урок2. Задание 3. Сделайте сценарий логина

---
+++ selenium/pl_yk/src/test/java/login/LoginTest.java
@@ -0,0 +1,34 @@
+package task2.login;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginTest {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 20);
+    }
+
+    @Test
+    public void loginTest() {
+        driver.navigate().to("http://localhost/litecard/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        driver.findElements(By.linkText(" You are now logged in as admin"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+    }
+}
